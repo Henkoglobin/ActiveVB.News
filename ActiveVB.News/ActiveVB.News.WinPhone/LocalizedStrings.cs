b92300a0e763ccada01d1382d3cb6ca1d70b4748
Henrik Ilgen <Henrik.Ilgen@ActiveVB.de>
2015-04-21 21:44:12 +0200

    Created solution

---
+++ ActiveVB.News/ActiveVB.News.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ActiveVB.News.WinPhone.Resources;
+
+namespace ActiveVB.News.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
